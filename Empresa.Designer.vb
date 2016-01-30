d95b84371eb0ca502ff5b3446e6af2840f51019a
yimydavid <yimysol@gmail.com>
2016-01-30 11:14:20 -0500

    Company/Entity form: populates listview with SELECT and Add new INSERT to list  with SELECT @@identity

--- Empresa.Designer.vb
+++ Empresa.Designer.vb
@@ -22,36 +22,39 @@ Partial Class frm_Empresa
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtCompany = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
         Me.tlsNuevo = New System.Windows.Forms.ToolStripButton()
         Me.tlsGuardar = New System.Windows.Forms.ToolStripButton()
+        Me.lstData = New System.Windows.Forms.ListView()
+        Me.ID = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Description = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ToolStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'txtCompany
         '
-        Me.TextBox1.Location = New System.Drawing.Point(97, 43)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(261, 20)
-        Me.TextBox1.TabIndex = 42
+        Me.txtCompany.Location = New System.Drawing.Point(97, 43)
+        Me.txtCompany.Name = "txtCompany"
+        Me.txtCompany.Size = New System.Drawing.Size(261, 20)
+        Me.txtCompany.TabIndex = 42
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(36, 46)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(48, 13)
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
         Me.Label1.TabIndex = 41
-        Me.Label1.Text = "Empreza"
+        Me.Label1.Text = "Entity"
         '
         'ToolStrip1
         '
         Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tlsNuevo, Me.tlsGuardar})
         Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
         Me.ToolStrip1.Name = "ToolStrip1"
-        Me.ToolStrip1.Size = New System.Drawing.Size(395, 25)
+        Me.ToolStrip1.Size = New System.Drawing.Size(398, 25)
         Me.ToolStrip1.TabIndex = 46
         Me.ToolStrip1.Text = "ToolStrip1"
         '
@@ -71,13 +74,39 @@ Partial Class frm_Empresa
         Me.tlsGuardar.Size = New System.Drawing.Size(69, 22)
         Me.tlsGuardar.Text = "Guardar"
         '
+        'lstData
+        '
+        Me.lstData.BackColor = System.Drawing.SystemColors.Window
+        Me.lstData.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ID, Me.Description})
+        Me.lstData.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.lstData.FullRowSelect = True
+        Me.lstData.GridLines = True
+        Me.lstData.HideSelection = False
+        Me.lstData.LabelEdit = True
+        Me.lstData.Location = New System.Drawing.Point(39, 84)
+        Me.lstData.Name = "lstData"
+        Me.lstData.Size = New System.Drawing.Size(319, 223)
+        Me.lstData.TabIndex = 47
+        Me.lstData.UseCompatibleStateImageBehavior = False
+        Me.lstData.View = System.Windows.Forms.View.Details
+        '
+        'ID
+        '
+        Me.ID.Text = "ID"
+        '
+        'Description
+        '
+        Me.Description.Text = "Description"
+        Me.Description.Width = 255
+        '
         'frm_Empresa
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(395, 96)
+        Me.ClientSize = New System.Drawing.Size(398, 319)
+        Me.Controls.Add(Me.lstData)
         Me.Controls.Add(Me.ToolStrip1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtCompany)
         Me.Controls.Add(Me.Label1)
         Me.Name = "frm_Empresa"
         Me.Text = "Empresa"
@@ -87,9 +116,12 @@ Partial Class frm_Empresa
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtCompany As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
     Friend WithEvents tlsNuevo As System.Windows.Forms.ToolStripButton
     Friend WithEvents tlsGuardar As System.Windows.Forms.ToolStripButton
+    Friend WithEvents lstData As System.Windows.Forms.ListView
+    Friend WithEvents ID As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Description As System.Windows.Forms.ColumnHeader
 End Class
--- Empresa.vb
+++ Empresa.vb
@@ -1,3 +1,71 @@
-﻿Public Class frm_Empresa
+﻿Imports System.Data.OleDb
+
+    Public Class frm_Empresa
+    Dim cnnOLEDB As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Expenses.accdb;")
+    Dim cmdOLEDB As New OleDbCommand
+    Dim cmdInsert As New OleDbCommand
+        Private Sub frm_Empresa_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cnnOLEDB.Open()
+        Dim cmd As New OleDbCommand("SELECT * FROM ECOMP", cnnOLEDB)
+        Dim dr As OleDbDataReader = cmd.ExecuteReader
+        Dim dtTable As DataTable = New DataTable()
+        dtTable.Columns.Add("ID", GetType(Integer))
+        dtTable.Columns.Add("Description", GetType(String))
+
+        While dr.Read
+            Dim new_item As New ListViewItem(dr.Item("CompanyID").ToString)
+            new_item.SubItems.Add(dr.Item("Description").ToString)
+            lstData.Items.Add(new_item)
+        End While
+
+        dr.Close()
+        cnnOLEDB.Close()
+        End Sub
+
+    Private Sub tlsGuardar_Click(sender As Object, e As EventArgs) Handles tlsGuardar.Click
+        Dim InsertQuery As String
+
+
+        Dim companyName As String
+        Dim auto_increment As Integer
+
+        'If there are empty required fields don't do anything
+        If (Not txtCompany.Text = "") Then
+            cnnOLEDB.Open()
+            'Variables to insert to database
+            companyName = txtCompany.Text
+
+            'Adding Record to listview
+            '  Dim new_item As New ListViewItem(.Text)
+
+
+            'Most examples have the following statement
+            'cmd.Parameters.AddWithValue("@id", 1) but it didn't work for me sofar I decided to do 
+            'the following
+
+            InsertQuery = "INSERT INTO [ECOMP] ([Description]) VALUES (" & "'" & companyName & "'" & ")"
+            Dim cmd As OleDbCommand = New OleDbCommand(InsertQuery, cnnOLEDB)
+
+            cmdOLEDB = New OleDbCommand(InsertQuery, cnnOLEDB)
+            cmdOLEDB.ExecuteNonQuery()
+            cmd.CommandText = "SELECT @@identity"
+            auto_increment = cmd.ExecuteScalar()
+            cnnOLEDB.Close()
+            lstData.Items.Add(New ListViewItem(New String() {auto_increment.ToString, companyName}))
+            'Me.Refresh()
+
+            MsgBox("Entity Saved!", MsgBoxStyle.Information, "Saved")
+            MsgBox(auto_increment)
+
+
+            txtCompany.Text = ""
+            txtCompany.Focus()
+
+        Else
+            MsgBox("Fill the Required Fields before Saving!", MsgBoxStyle.Critical, "Empty Required Fields")
+        End If
+
+    End Sub
+
 
 End Class
